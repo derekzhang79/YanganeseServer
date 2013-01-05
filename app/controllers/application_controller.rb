583c859888876165beca066cc783247f61dbe12e
Michael Yang <my2292@columbia.edu>
2013-01-05 14:34:18 -0500

    changed root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+    redirect_to 'http://mikkyang.com/project/yanganese'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 YanganeseServer::Application.routes.draw do
+  root to: 'application#index'
   resources :quizzes
   resources :questions, only: [:create, :update, :destroy]
 end
